a3a4304eb8623145b5bc9087edb8372b421a4d4e
motoloto <motoloto0329@gmail.com>
2017-02-19 23:53:32 +0800

    first commit

---
+++ src/main/java/com/mkyong/common/Performance.java
@@ -0,0 +1,45 @@
+package com.mkyong.common;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class Performance implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String season1;
+	private String season2;
+	private String season3;
+	private String season4;
+	public String getSeason1() {
+		return season1;
+	}
+	public void setSeason1(String season1) {
+		this.season1 = season1;
+	}
+	public String getSeason2() {
+		return season2;
+	}
+	public void setSeason2(String season2) {
+		this.season2 = season2;
+	}
+	public String getSeason3() {
+		return season3;
+	}
+	public void setSeason3(String season3) {
+		this.season3 = season3;
+	}
+	public String getSeason4() {
+		return season4;
+	}
+	public void setSeason4(String season4) {
+		this.season4 = season4;
+	}
+	
+	
+	
+}
